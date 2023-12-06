7deab2dbf0c2018a2373d0b34f9a28fc70e17dcc
Himanshu Arya <stararya1234@gmail.com>
2023-12-06 15:52:59 +0530

    Onboarding Page

--- app/src/main/java/com/example/news99/presentation/onboarding/Page.kt
+++ app/src/main/java/com/example/news99/presentation/onboarding/Page.kt
@@ -1,3 +1,28 @@
 package com.example.news99.presentation.onboarding
 
-data class Page()
+import androidx.annotation.DrawableRes
+import com.example.news99.R
+
+data class Page(
+    val title: String,
+    val description: String,
+    @DrawableRes val image: Int
+)
+
+val pages = listOf(
+    Page(
+        "lorem ipsum is a dummy",
+        "baka baka dummy dummy for the news99",
+        R.drawable.onboarding1
+    ),
+    Page(
+        "lorem ipsum is a dummy",
+        "baka baka dummy dummy for the news99",
+        R.drawable.onboarding2
+    ),
+    Page(
+        "lorem ipsum is a dummy",
+        "baka baka dummy dummy for the news99",
+        R.drawable.onboarding3
+    )
+)
