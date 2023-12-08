b4b22d9fb753e94f5a5b5611f6c4019ac6881c10
Himanshu Arya <stararya1234@gmail.com>
2023-12-08 22:31:31 +0530

    setting up the news usecases and news viewmodel

--- app/src/main/java/com/example/news99/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/news99/presentation/home/HomeViewModel.kt
@@ -1,8 +1,18 @@
 package com.example.news99.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.example.news99.domain.usecases.news.NewsUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel:ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val newsUseCases: NewsUseCases
+):ViewModel() {
+
+    val news = newsUseCases.getNews(
+        sources = listOf("bbc-news","abc-news")
+    ).cachedIn(viewModelScope)
 }
---
+++ app/src/main/java/com/example/news99/presentation/shared/ArticleCard.kt
@@ -0,0 +1,19 @@
+package com.example.news99.presentation.shared
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.news99.domain.model.Article
+
+
+@Composable
+fun ArticleCard(
+    modifier: Modifier= Modifier,
+    article:Article,
+    onClick: ()->Unit
+){
+
+    Row() {
+
+    }
+}
