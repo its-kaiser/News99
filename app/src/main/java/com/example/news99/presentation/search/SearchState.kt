04f390c0a199ac0d1eef6114b863c102923eac21
Himanshu Arya <stararya1234@gmail.com>
2023-12-09 17:29:32 +0530

    search state added

--- app/src/main/java/com/example/news99/presentation/search/SearchState.kt
+++ app/src/main/java/com/example/news99/presentation/search/SearchState.kt
@@ -1,4 +1,11 @@
 package com.example.news99.presentation.search
 
-class SearchState {
+import androidx.paging.PagingData
+import com.example.news99.domain.model.Article
+import kotlinx.coroutines.flow.Flow
+
+data class SearchState(
+    val searchQuery:String ="",
+    val articles : Flow<PagingData<Article>>? = null
+) {
 }
